2f7fc2c44079ca7fe99547aa5ae68a23b89ead6c
Delian Petrov <petrovd@uci.edu>
2014-07-15 12:19:24 -0700

    fixed javascript not loading
    Turbolinks was the issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,11 @@
 //= require bootstrap
 //= require_tree .
 
-// Collapsable Sidebar
-$(document).ready(function() {
+var collapse = function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
-});
+};
+
+$(document).ready(collapse);
+$(document).on('page:load', collapse);
