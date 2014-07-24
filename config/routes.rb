2360fc4583f266303031b4b19485012e6df69251
Delian Petrov <petrovd@uci.edu>
2014-07-23 19:48:57 -0700

    generate Responses Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'responses/index'
+
+  get 'responses/show'
+
+  get 'responses/new'
+
+  get 'responses/edit'
+
+  get 'responses/delete'
+
   get 'visuals/index'
   get 'modules/index'
 
