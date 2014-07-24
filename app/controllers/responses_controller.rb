ad1748c5928ded7cdc52c5a229b69d0caa550344
Delian Petrov <petrovd@uci.edu>
2014-07-23 20:17:09 -0700

    created Responses#Index & #Create

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,11 +1,24 @@
 class ResponsesController < ApplicationController
   def index
+    @responses = Response.all
   end
 
   def show
+    @response = Response.find(params[:id])
   end
 
   def new
+    @response = Response.new
+  end
+  
+  def create
+    @response = Response.new(response_params)
+    
+    if @response.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -13,4 +26,10 @@ class ResponsesController < ApplicationController
 
   def delete
   end
+  
+  private 
+  
+  def response_params
+    params.require(:response).permit(:input, :question_id)
+  end 
 end
