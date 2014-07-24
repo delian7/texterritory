af06d57e8d6458d12c438719fd9da661cabe134a
Delian Petrov <petrovd@uci.edu>
2014-07-23 20:22:49 -0700

    added user_id to responses model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718043253) do
+ActiveRecord::Schema.define(version: 20140724031923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140718043253) do
     t.string   "input"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
